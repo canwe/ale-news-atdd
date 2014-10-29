e31f31c34a485722902b27d477ddc6a07332ce92
Stephan Schwab <sns@caimito.net>
2014-10-29 09:28:07 +0800

    build script

--- make-world.sh
+++ make-world.sh
@@ -9,11 +9,17 @@ if test $? -eq 0; then
 	echo "[INFO] Making IOS Application"
 	cd ../ios/ALE-News
 	xcodebuild build -sdk iphonesimulator8.1 -configuration Debug CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
-
 	if test $? -eq 0; then
 		SUCCESS_IOS=true
 	else
 		SUCCESS_IOS=false
+	fi
+	
+	cucumber features
+	if test $? -eq 0; then
+		SUCCESS_IOS_CUCUMBER=true
+	else
+		SUCCESS_IOS_CUCUMBER=false
 	fi
 else
 	SUCCESS_WEB=false
@@ -32,6 +38,13 @@ if [ $SUCCESS_IOS == true ]; then
 	BUILD_SUCCESS=true
 else
 	echo "[INFO] IOS Application ................ FAILURE"
+	BUILD_SUCCESS=false
+fi
+if [ SUCCESS_IOS_CUCUMBER == true ]; then
+	echo "[INFO] IOS Application Cucumber ....... SUCCESS"
+	BUILD_SUCCESS=true
+else
+	echo "[INFO] IOS Application Cucumber ....... FAILURE"
 	BUILD_SUCCESS=false
 fi
 echo "[INFO] ------------------------------------------------------------------------"

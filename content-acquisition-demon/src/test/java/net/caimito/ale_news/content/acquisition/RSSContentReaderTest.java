b30b51a81530f0f7540329ebeef4b77456ac43d5
Stephan Schwab <sns@caimito.net>
2015-02-07 13:09:45 +0100

    rss content reader test

--- content-acquisition-demon/src/test/java/net/caimito/ale_news/content/acquisition/RSSContentReaderTest.java
+++ content-acquisition-demon/src/test/java/net/caimito/ale_news/content/acquisition/RSSContentReaderTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.*;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -13,12 +16,19 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class RSSContentReaderTest {
 
     @Test
-    public void returnFirstItemFromFeed() throws MalformedURLException {
+    public void returnFirstItemFromFeed() throws MalformedURLException, ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss") ;
+
         RSSContentReader reader = new RSSContentReader() ;
         Content content = reader.read(new URL(String.format("file://%s/%s", System.getProperty("user.dir"), "src/test/resources/content.rss"))) ;
+
         assertThat(content, is(notNullValue())) ;
         assertThat(content.getBodyContentType(), is(ContentType.HTML)) ;
         assertThat(content.getBody(), is(notNullValue())) ;
         assertThat(content.getSourceLocation().toExternalForm(), is(equalTo("http://trustartist.com/2015/01/27/pair-programming-economics/"))) ;
+        assertThat(content.getTitle(), is(equalTo("Pair Programming Economics"))) ;
+        assertThat(content.getPublishedDate(), is(dateFormat.parse("2015-01-26 23:25:35"))) ;
+        assertThat(content.getAuthor(), is(equalTo("Olaf Lewitz"))) ;
+        assertThat(content.getCategories(), hasItems("Agile with a Purpose", "Conference", "Programming", "pair programming", "TDD")) ;
     }
 }

9303e35ee7e0bdf498ad8febf2e7e2fe98fbfc6d
Stephan Schwab <sns@caimito.net>
2015-03-05 09:57:48 +0100

    display discussions

--- alenews/alenews-web/src/main/java/alenews/Application.java
+++ alenews/alenews-web/src/main/java/alenews/Application.java
@@ -13,4 +13,7 @@ public class Application extends SpringBootServletInitializer {
         return application;
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- alenews/alenews-web/src/main/webapp/js/controllers.js
+++ alenews/alenews-web/src/main/webapp/js/controllers.js
@@ -4,7 +4,7 @@ var controllers = angular.module('controllers', []);
 
 controllers.controller('contentListController', ['$scope', '$http', '$sce',
     function ($scope, $http, $sce) {
-        $http.get('http://172.16.110.151:8080/alenews-web/content').success(function(data) {
+        $http.get('/alenews-web/content').success(function(data) {
             $scope.articles = data;
         });
     }]);
